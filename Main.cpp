5b18fb62dfbcc893a0fdc61d166db08290d150c6
cdOut <88325488+cdOut@users.noreply.github.com>
2024-05-28 12:25:06 +0200

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+#ifdef NDEBUG
+#include <Windows.h>
+#endif
+
+#ifdef _DEBUG
+int main()
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#endif
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
