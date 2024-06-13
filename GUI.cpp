238a88d7c0aceaca32234a1250ade19f455025c8
Сергей Виноградов <angrycomik@gmail.com>
2024-06-13 20:43:33 +0200

    added Screenshot button

--- GUI.cpp
+++ GUI.cpp
@@ -8,6 +8,15 @@ void DrawGUI(sf::RenderWindow& window, SimulationPlane& simulationPlane, bool& i
             NextFrame(simulationPlane, clock, pausedTime);
         }
     }
+    if (ImGui::Button("Make Screenshot")) {
+        sf::Texture texture;
+        texture.create(window.getSize().x, window.getSize().y);
+        texture.update(window);
+        if (texture.copyToImage().saveToFile("screen.png"))
+        {
+            CopyTextureToClipboard(texture);
+        }
+    }
 
     ImGui::End();
 
@@ -54,6 +63,50 @@ void Rotate(SimulationPlane& simulationPlane, sf::Event& event, bool &isDraggin,
         {
             if (!sf::Mouse::isButtonPressed(sf::Mouse::Left)) isDraggin = false;
         }
+    }
+}
+
+void CopyTextureToClipboard(const sf::Texture& texture) {
+    sf::Image image = texture.copyToImage();
+    int width = image.getSize().x;
+    int height = image.getSize().y;
+
+    BITMAPINFO bmi;
+    ZeroMemory(&bmi, sizeof(BITMAPINFO));
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = width;
+    bmi.bmiHeader.biHeight = -height; // inaczej odwrocony
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    const sf::Uint8* pixels = image.getPixelsPtr();
+
+    // konwersja z rgb na bgr
+    std::vector<sf::Uint8> converted(width * height * 4);
+    for (int i = 0; i < width * height; i++) {
+        converted[i * 4 + 0] = pixels[i * 4 + 2]; 
+        converted[i * 4 + 1] = pixels[i * 4 + 1]; 
+        converted[i * 4 + 2] = pixels[i * 4 + 0]; 
+        converted[i * 4 + 3] = pixels[i * 4 + 3];
+    }
+
+    if (OpenClipboard(NULL)) {
+        EmptyClipboard();
+
+        HGLOBAL bitmap = GlobalAlloc(GHND, sizeof(BITMAPINFOHEADER) + (width * height * 4));
+        if (bitmap) {
+            void* lock = GlobalLock(bitmap);
+            if (lock) {
+                memcpy(lock, &bmi.bmiHeader, sizeof(BITMAPINFOHEADER));
+                memcpy((BYTE*)lock + sizeof(BITMAPINFOHEADER), converted.data(), width * height * 4);
+
+                GlobalUnlock(bitmap);
+                SetClipboardData(CF_DIB, bitmap);
+            }
+            GlobalFree(bitmap);
+        }
+        CloseClipboard();
     }
 }
 
--- GUI.hpp
+++ GUI.hpp
@@ -6,12 +6,14 @@
 #include "imgui-SFML.h"
 #include <SFML/Graphics.hpp>
 #include "SimulationPlane.hpp"
+#include <Windows.h>
 
 
 
 void DrawGUI(sf::RenderWindow& window, SimulationPlane& simulationPlane, bool& isAnimationRunning, sf::Clock& clock, sf::Time& pausedTime, bool& isPaused);
 void NextFrame(SimulationPlane& simulationPlane, sf::Clock& clock, sf::Time& pausedTime);
 void Rotate(SimulationPlane& simulationPlane, sf::Event& event, bool &isDraggin, sf::Vector2i& mouse_pos);
+void CopyTextureToClipboard(const sf::Texture& texture);
 
 
 
